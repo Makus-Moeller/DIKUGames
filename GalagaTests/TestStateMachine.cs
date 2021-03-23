9b4c5911114dc367ed813767df758a8fac1495e1
Makus-Moeller <markus.m@live.dk>
2021-03-23 17:04:47 +0100

    TEST for GamestateEvents

---
+++ GalagaTests/TestStateMachine.cs
@@ -0,0 +1,56 @@
+using NUnit.Framework;
+using Galaga;
+
+namespace GalagaTests {
+    [TestFixture]
+    public class StateMachineTesting {
+        private StateMachine stateMachine;
+        
+
+        public StateMachineTesting() {
+            DIKUArcade.Window.CreateOpenGLContext();
+            GalagaBus.GetBus();
+
+
+        }
+        [SetUp]
+        public void SetUpMethod() {
+            stateMachine = new StateMachine();
+            // Here you should:
+            // (1) Initialize a GalagaBus with proper GameEventTypes
+            // (2) Instantiate the StateMachine
+            // (3) Subscribe the GalagaBus to proper GameEventTypes
+            // and GameEventProcessors
+            
+        }
+
+        [Test]
+        public void TestInitialState() {
+            Assert.That(stateMachine.ActiveState, Is.InstanceOf<MainMenu>());
+        }
+        
+        [Test]
+        public void TestEventGamePaused() {
+            GalagaBus.GetBus().RegisterEvent(
+                GameEventFactory<object>.CreateGameEventForAllProcessors(
+                    GameEventType.GameStateEvent,
+                    this,
+                    "CHANGE_STATE",
+                    "GAME_PAUSED", ""));
+            GalagaBus.GetBus().ProcessEventsSequentially();
+            Assert.That(stateMachine.ActiveState, Is.InstanceOf<GamePaused>());
+        }
+
+         [Test]
+        public void TestEventGameRunning() {
+            GalagaBus.GetBus().RegisterEvent(
+                GameEventFactory<object>.CreateGameEventForAllProcessors(
+                    GameEventType.GameStateEvent,
+                    this,
+                    "CHANGE_STATE",
+                    "GAME_RUNNING", ""));
+            GalagaBus.GetBus().ProcessEventsSequentially();
+            Assert.That(stateMachine.ActiveState, Is.InstanceOf<GamePaused>());
+        }
+    }
+}
