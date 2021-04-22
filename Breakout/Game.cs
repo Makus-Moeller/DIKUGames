19922c39167f35955d87b3aabeb6fa4a63da6331
Makus-Moeller <markus.m@live.dk>
2021-04-22 15:15:35 +0200

    Assets + simple game class

--- Breakout/Game.cs
+++ Breakout/Game.cs
@@ -1,10 +1,48 @@
 using System;
 using DIKUArcade;
+using DIKUArcade.GUI;
+using DIKUArcade.Input;
 
 namespace Breakout {
     public class Game : DIKUGame {
+        public Game(WindowArgs winArgs) : base(winArgs)  {
+            window.SetKeyEventHandler(keyHandler);
+            window.SetClearColor(System.Drawing.Color.BlueViolet);
+        }
         
+        private void keyHandler(KeyboardAction action, KeyboardKey key) {
+            if (action == KeyboardAction.KeyPress) {
+                switch (key) {
+                    case KeyboardKey.Num_1:
+                        window.SetClearColor(System.Drawing.Color.Beige);
+                        break;
+                    case KeyboardKey.Num_2:
+                        window.SetClearColor(System.Drawing.Color.Coral);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else if (action == KeyboardAction.KeyRelease) {
+                switch (key) {
+                    case KeyboardKey.Escape:
+                        window.CloseWindow();
+                        break;
+                    default:
+                        break;
+                }
+            }
+            
 
+        }
+        public override void Render()
+        {
+            
+        }
 
+        public override void Update()
+        {
+            
+        }
     }
 }
