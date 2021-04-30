6f0d0dc7513576ab5baa534ead3cc67aa5632147
Makus-Moeller <markus.m@live.dk>
2021-04-30 16:52:42 +0200

    Delete entity field

--- Breakout/Blocks/PowerUpBlock.cs
+++ Breakout/Blocks/PowerUpBlock.cs
@@ -7,7 +7,6 @@ namespace Breakout.Blocks {
 
     //PowerUp klassen har et ekstra fiels som er et PowerUpItem. En dynamic shape
     public class PowerUpBlock : AtomBlock {
-        private Entity PowerUpItem;
         public PowerUpBlock(Shape shape, IBaseImage image) : base(shape, image) {
             PowerUpItem = new Entity(new DynamicShape(Shape.Position, new Vec2F(0.02f, 0.02f), 
                 new Vec2F(0.0f, -0.01f)), 
